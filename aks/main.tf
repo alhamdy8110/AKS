2e7bf0ccbeeee67eded8cfd3265542e93913f652
abdulnaser.alhamdy <abdulnaser.alhamdy@yahoo.com>
2025-04-03 22:12:41 +0200

    change the flow and deploy the k8s

--- aks/main.tf
+++ aks/main.tf
@@ -45,25 +45,3 @@ resource "azurerm_kubernetes_cluster_node_pool" "userpool" {
   vm_size               = "Standard_A2_v2" 
   node_count            = 1
 }
-
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.k8s.kube_config.0.host
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
-  token                  = azurerm_kubernetes_cluster.k8s.kube_config.0.access_token
-}
-
-resource "kubernetes_storage_class" "azurefile" {
-  metadata {
-    name = var.resource_storage_file_name
-  }
-
-  storage_provisioner = "file.csi.azure.com"
-  reclaim_policy     = "Delete"  # Or "Retain" if you want to keep storage after PVC deletion
-  volume_binding_mode = "Immediate"
-
-  parameters = {
-    skuName = "Standard_LRS"  # Options: Standard_LRS, Standard_GRS, Standard_ZRS, Premium_LRS
-  }
-
-  depends_on = [azurerm_kubernetes_cluster.k8s, azurerm_kubernetes_cluster_node_pool.userpool]
-}
