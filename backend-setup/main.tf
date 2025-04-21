a836f810aaea1ea7da2e4727846737d90b87ad17
abdulnaser.alhamdy <abdulnaser.alhamdy@yahoo.com>
2025-04-21 10:58:42 +0200

    storage_account_name  = azurerm_storage_account.tfstate.name

--- backend-setup/main.tf
+++ backend-setup/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_storage_account" "tfstate" {
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.tfstate.id
+  storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
 
